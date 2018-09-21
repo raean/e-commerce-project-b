772f370dc6de74cc9f7354319cb3fe9be238b81e
Rae <35583646+raean@users.noreply.github.com>
2018-09-21 12:57:19 -0400

    Add files via upload

---
+++ ProjB/src/ctrl/Time.java
@@ -0,0 +1,54 @@
+package ctrl;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Brain;
+
+@WebServlet("/Time.do")
+public class Time extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (request.getParameter("calc") == null)
+		  {
+		     this.getServletContext().getRequestDispatcher("/Time.html").forward(request, response);
+		  }
+		  else
+		  {
+		     Brain model = new Brain();
+		     try
+		     {
+		        String time = model.doTime();
+		        response.setContentType("text/html");
+		        Writer out = response.getWriter();
+		        String html = "<html><body>";
+		        html += "<p><a href='Dash.do'>Back to Dashboard</a></p>";
+		        html += "<p>Server Time: " + time + "</p>";
+		        html += "</body></html>";
+		        out.write(html);
+		     }
+		     catch (Exception e)
+		     {
+		        response.setContentType("text/html");
+		        Writer out = response.getWriter();
+		        String html = "<html><body>";
+		        html += "<p><a href=' Time.do'>Back to Dashboard</a></p>";
+		        html += "<p>Error " + e.getMessage() + "</p>";
+		        out.write(html);
+		     }
+		  }
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
